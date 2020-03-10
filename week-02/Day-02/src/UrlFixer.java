2680cb570067f00ae3f3c12e5bdcfe4ed3acdd57
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-03-10 14:32:21 +0100

    uploading again

--- week-02/Day-02/src/UrlFixer.java
+++ week-02/Day-02/src/UrlFixer.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class UrlFixer {
+    public static void main(String[] args) {
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+
+        // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+        // Also, the URL is missing a crucial component, find out what it is and insert it too!
+        // Try to solve it more than once using different String functions!
+
+        char character = ':';
+        StringBuffer colon = new StringBuffer(url);
+        colon.insert(5, character);
+
+        String newUrl = "";
+        if (url.contains("bots")){
+            newUrl = colon.toString().replace("bots", "odds");
+        }
+        System.out.println(newUrl);
+
+    }
 }
--- week-02/Day-02/src/takesLonger.java
+++ week-02/Day-02/src/takesLonger.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class takesLonger {
+    public static void main(String[] args) {
+        String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+        // When saving this quote a disk error has occurred. Please fix it.
+        // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+        // Using pieces of the quote variable (instead of just redefining the string)
+
+        StringBuilder quote2 = new StringBuilder(quote);
+        String toAdd = " always takes longer than";
+        String itEquals = "It";
+
+        int intQuote = quote.indexOf(itEquals);
+        System.out.println(intQuote);
+
+
+        quote2.insert(intQuote+itEquals.length(), toAdd);
+        System.out.println(quote2);
+
+    }
 }
