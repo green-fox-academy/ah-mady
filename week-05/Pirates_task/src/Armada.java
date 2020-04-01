82bfb38fdb20100205edcdc7ef7484bd0c412090
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-04-01 14:06:20 +0200

    half of the pirate task is ready upto armadapirates task is ready

--- week-05/Pirates_task/src/Armada.java
+++ week-05/Pirates_task/src/Armada.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Armada {
+
+  private List<Ship> ships= new ArrayList<>();
+
+  public boolean war (Armada enemy){
+    int indexOfThis = 0;
+    int indexOfEnemy =0;
+
+    while (indexOfThis < ships.size() && indexOfEnemy < enemy.ships.size()){
+      boolean thisWins = ships.get(indexOfThis).battle(enemy.ships.get(indexOfEnemy));
+      if (thisWins){
+        indexOfEnemy++;
+      } else {
+        indexOfThis++;
+      }
+    }
+    return indexOfThis < indexOfEnemy;
+  }
+
+  public void addShip(Ship ship){
+    ships.add(ship);
+  }
+
 }
--- week-05/Pirates_task/src/WarrApp.java
+++ week-05/Pirates_task/src/WarrApp.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class WarrApp {
+  public static void main(String[] args) {
+    Random random = new Random();
+    Armada armada1 = new Armada();
+    Armada armada2 = new Armada();
+
+    int ships = random.nextInt(9)+1;
+
+    for (int i = 0; i < ships ; i++) {
+      boolean toFirstArmada = random.nextBoolean();
+
+      Ship ship = new Ship();
+      ship.fillShip();
+
+      if (toFirstArmada) {
+        armada1.addShip(ship);
+      } else {
+        armada2.addShip(ship);
+      }
+    }
+    boolean firstWins = armada1.war(armada2);
+    System.out.println((firstWins? "First" : "Second")+" armada won the battle");
+
+
+  }
+
 }
